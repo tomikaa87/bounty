7bbee0c7c4aa7940da483641da94a1a509f5c829
Tamas Karpati <tomikaa87@gmail.com>
2016-07-12 22:16:08 +0200

    Make - return decltype(auto)

--- BountyHunt/Make.h
+++ BountyHunt/Make.h
@@ -12,7 +12,7 @@ namespace bounty
 {
 
 template <typename T, typename... Args>
-constexpr T make(Args&&... args)
+constexpr decltype(auto) make(Args&&... args)
 {
     return T{ std::forward<Args>(args)... };
 }
