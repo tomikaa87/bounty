1300f3a08d10812b065199b2604974bf6591065f
Tamas Karpati <tomikaa87@gmail.com>
2016-07-12 21:32:15 +0200

    Make - Add implementation + tests

---
+++ BountyHunt/Make.h
@@ -0,0 +1,20 @@
+//
+// Constructs an object with the arguments passed into the constructor.
+//
+// Created by Tamas Karpati on 2016-07-12.
+//
+
+#pragma once
+
+#include <type_traits>
+
+namespace bounty
+{
+
+template <typename T, typename... Args>
+constexpr T make(Args&&... args)
+{
+    return T{ std::forward<Args>(args)... };
+}
+
+}
---
+++ BountyHunt/MakeTests.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Tamas Karpati on 2016-07-12.
+//
+
+#include "gmock/gmock.h"
+#include "Make.h"
+
+struct test_data
+{
+    test_data() = default;
+
+    explicit test_data(int a)
+        : sum(a)
+    {}
+
+    test_data(int a, double b)
+        : sum(a + static_cast<int>(b))
+    {}
+
+    test_data(int a, int b, const test_data& c)
+        : sum(a + b + c.sum)
+    {}
+
+    int sum = 123;
+};
+
+TEST(MakeTests, MakeObjectNoArguments)
+{
+    auto&& test = bounty::make<test_data>();
+    EXPECT_EQ(123, test.sum);
+}
+
+TEST(MakeTests, MakeObjectOneArgument)
+{
+    auto&& test = bounty::make<test_data>(1);
+    EXPECT_EQ(1, test.sum);
+}
+
+TEST(MakeTests, MakeObjectTwoArguments)
+{
+    auto&& test = bounty::make<test_data>(1, 2.0);
+    EXPECT_EQ(3, test.sum);
+}
+
+TEST(MakeTests, MakeObjectThreeArguments)
+{
+    test_data test123;
+    auto&& test = bounty::make<test_data>(1, 2, test123);
+    EXPECT_EQ(126, test.sum);
+}
